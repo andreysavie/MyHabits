191faf34d72fd88339957ccd5358c328245ba2d6
Andrey Rybalkin <andreysavie@ya.ru>
2022-03-10 18:02:12 +0300

    appDelegate configured

--- MyHabits/AppDelegate.swift
+++ MyHabits/AppDelegate.swift
@@ -11,71 +11,60 @@ import CoreData
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+    var window: UIWindow?
 
+    let attributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 12)]
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+    
+        window = UIWindow(frame: UIScreen.main.bounds)
+        
+        let tabBarController = UITabBarController()
+                        
+        let habitsViewController = HabitsViewController()
+        habitsViewController.view.backgroundColor = .white
+        let habitsNavigationController = UINavigationController(rootViewController: habitsViewController)
+        
+        let infoViewController = InfoViewController()
+        infoViewController.view.backgroundColor = .white
+        let infoNavigationController = UINavigationController(rootViewController: infoViewController)
+        
+        let appearance = UINavigationBarAppearance()
+        appearance.configureWithOpaqueBackground()
+        appearance.backgroundColor = Colors.navigationBarColor
+                
+        habitsNavigationController.tabBarItem = UITabBarItem(
+            title: Labels.habitsTabBarTitle,
+            image: UIImage(systemName: "rectangle.grid.1x2"),
+            selectedImage: UIImage(systemName: "rectangle.grid.1x2.fill")
+        )
+        habitsNavigationController.tabBarItem.setTitleTextAttributes(attributes,for: .normal)
+        habitsNavigationController.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20)]
+        habitsNavigationController.navigationBar.topItem?.title = Labels.habitsNavigationControllerTitle
+        habitsNavigationController.navigationBar.barTintColor = Colors.navigationBarColor
+        habitsNavigationController.navigationBar.standardAppearance = appearance
+        habitsNavigationController.navigationBar.scrollEdgeAppearance = habitsNavigationController.navigationBar.standardAppearance
+        
+        infoNavigationController.tabBarItem = UITabBarItem(
+            title: Labels.infoTabBarTitle,
+            image: UIImage(systemName: "info.circle"),
+            selectedImage: UIImage(systemName:"info.circle.fill")
+        )
+        infoNavigationController.tabBarItem.setTitleTextAttributes(attributes, for: .normal)
+        infoNavigationController.navigationBar.titleTextAttributes = [NSAttributedString.Key.font: UIFont.systemFont(ofSize: 20)]
+        infoNavigationController.navigationBar.topItem?.title = Labels.infoNavigationControllerTitle
+        infoNavigationController.navigationBar.barTintColor = Colors.navigationBarColor
+        infoNavigationController.navigationBar.standardAppearance = appearance
+        infoNavigationController.navigationBar.scrollEdgeAppearance = infoNavigationController.navigationBar.standardAppearance
+        
+        tabBarController.viewControllers = [habitsNavigationController, infoNavigationController]
+        tabBarController.tabBar.backgroundColor = UIColor.systemGray5
+                        
+        window?.rootViewController = tabBarController
+        window?.makeKeyAndVisible()
+        
         return true
     }
-
-    // MARK: UISceneSession Lifecycle
-
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
-
-    // MARK: - Core Data stack
-
-    lazy var persistentContainer: NSPersistentContainer = {
-        /*
-         The persistent container for the application. This implementation
-         creates and returns a container, having loaded the store for the
-         application to it. This property is optional since there are legitimate
-         error conditions that could cause the creation of the store to fail.
-        */
-        let container = NSPersistentContainer(name: "MyHabits")
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-            if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                 
-                /*
-                 Typical reasons for an error here include:
-                 * The parent directory does not exist, cannot be created, or disallows writing.
-                 * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                 * The device is out of space.
-                 * The store could not be migrated to the current model version.
-                 Check the error message to determine what the actual problem was.
-                 */
-                fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
-        })
-        return container
-    }()
-
-    // MARK: - Core Data Saving support
-
-    func saveContext () {
-        let context = persistentContainer.viewContext
-        if context.hasChanges {
-            do {
-                try context.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nserror = error as NSError
-                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
-            }
-        }
-    }
-
 }
 
---
+++ MyHabits/Constants.swift
@@ -0,0 +1,33 @@
+//
+//  Constants.swift
+//  MyHabits
+//
+//  Created by Андрей Рыбалкин on 10.03.2022.
+//
+
+import Foundation
+import UIKit
+
+public struct Colors {
+    
+    static let lightGrayColoe = UIColor(red: 242, green: 242, blue: 247, alpha: 1)
+    static let purpleColor = UIColor(red: 161, green: 22, blue: 204, alpha: 1)
+    static let blueColor = UIColor(red: 41, green: 109, blue: 255, alpha: 1)
+    static let greenColor = UIColor(red: 29, green: 179, blue: 34, alpha: 1)
+    static let indigoColor = UIColor(red: 98, green: 54, blue: 255, alpha: 1)
+    static let orangeColor = UIColor(red: 255, green: 159, blue: 79, alpha: 1)
+    static let navigationBarColor = UIColor(red: 249, green: 249, blue: 249, alpha: 0.94)
+    
+}
+
+public struct Labels {
+    
+    static let habitsNavigationControllerTitle = "Сегодня"
+    static let infoNavigationControllerTitle = "Информация"
+    
+    static let habitsTabBarTitle = "Привычки"
+    static let infoTabBarTitle = "Информация"
+
+    
+}
+
