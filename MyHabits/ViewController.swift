3fda825cbc5fecb5ecd6d0467cb7a961a56b910d
Andrey Rybalkin <andreysavie@ya.ru>
2022-03-10 17:11:24 +0300

    Initial Commit

---
+++ MyHabits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Андрей Рыбалкин on 10.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
